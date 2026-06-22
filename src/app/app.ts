b158cd3cf159f0047f90109c56109d09613f9ab8
Christela Mba <mbachristela@gmail.com>
2026-06-22 04:25:18 +0100

    feat(temoignages): section témoignages avec étoiles dynamiques

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header'
-import { Hero } from './components/hero/hero'
-import { Menu } from './components/menu/menu'
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { Menu } from './components/menu/menu';
+import { Temoignages } from './components/temoignages/temoignages';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, Menu],
+  imports: [RouterOutlet, Header, Hero, Menu, Temoignages],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
